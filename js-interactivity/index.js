e6f147abd6051d4347e552ec12d009b11c4ca925
Estefania Sosa <estefi.monumental@gmail.com>
2022-07-27 17:38:26 -0400

    Intermidiate lab finished

--- js-interactivity/index.js
+++ js-interactivity/index.js
@@ -25,14 +25,22 @@ document.querySelector('form').addEventListener("submit", addMovie)
 function crossOffMovie (event) {
     event.target.classList.toggle("checked")
     if (event.target.classList.contains("checked")) {
-        message.textContent = "Movie watched!"
+        message.textContent = `${event.target.textContent} watched!`
     } else {
-        message.textContent = "Movie added back!"
+        message.textContent = `${event.target.textContent} added back!`
     }
+    revealMessage()
 }
 
 function deleteMovie (event) {
     event.target.parentNode.remove()
-    message.textContent = "Movie deleted!"
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+    revealMessage()
 }
 
+function revealMessage () {
+    message.classList.remove("hide")
+    setTimeout(() => {
+        message.classList.add("hide")
+    }, 1000)
+}
